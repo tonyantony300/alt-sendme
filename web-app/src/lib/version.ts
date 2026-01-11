228b1b236035eff66e320eb7131ef6fd4a1c4fd3
Enoch Klu <eenoch@delalify.com>
2026-01-11 09:13:27 +0000

    chore(web-app): update `package.json` path

--- web-app/src/lib/version.ts
+++ web-app/src/lib/version.ts
@@ -1,4 +1,4 @@
-import packageJson from '../../package.json';
+import packageJson from '../../../package.json';
 
 export const VERSION = packageJson.version;
 export const VERSION_DISPLAY = `v${VERSION}`;
