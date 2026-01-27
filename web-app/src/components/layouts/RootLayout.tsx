6b414e1500d08cbc6a7d89616d474e003f5581a8
tony <tonyantony300@gmail.com>
2026-01-28 01:58:16 +0530

    fix(layout): update translation key for TitleBar in RootLayout component

--- web-app/src/components/layouts/RootLayout.tsx
+++ web-app/src/components/layouts/RootLayout.tsx
@@ -5,11 +5,11 @@ import { TitleBar } from '../TitleBar'
 import { useTranslation } from '@/i18n'
 
 export function RootLayout() {
-	const { t } = useTranslation()
+	const { t } = useTranslation('common')
 	return (
 		<AppProviders>
 			<main className="h-screen flex flex-col relative glass-background select-none bg-background">
-				{IS_LINUX && <TitleBar title={t('common:appTitle')} />}
+				{IS_LINUX && <TitleBar title={t('appTitle')} />}
 
 				{IS_MACOS && (
 					<div className="absolute w-full h-10 z-10" data-tauri-drag-region />
