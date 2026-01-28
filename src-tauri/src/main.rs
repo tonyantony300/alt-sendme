95400ca6e78f837c96b362fd1fe3f15b43ea13ff
Arane Aimer <araneaimer@gmail.com>
2026-01-28 15:51:14 +0530

    feat(updater): add tauri-plugin-updater backend support
    - Add 	auri-plugin-updater dep
    - Register updater plugin in src-tauri/src/main.rs
    - Configure upstream endpoint in 	auri.conf.json
    
    > Note: the latest.json file will be generated by tauri-actions GitHub CI once a new release is made

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -65,6 +65,7 @@ fn main() {
     );
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             if let Some(window) = app.get_webview_window("main") {
                 let _ = window.show();
