d9ecf926f73cb54f28b2fc24ec75a33df4ad6175
hcodename47 <hcodename47@gmail.com>
2026-02-03 20:20:52 +0900

    Add Hungarian translation

--- web-app/src/components/LanguageSwitcher.tsx
+++ web-app/src/components/LanguageSwitcher.tsx
@@ -28,6 +28,7 @@ const LANGUAGES = [
 	{ value: 'tr', label: 'Türkçe' },
 	{ value: 'no', label: 'Norsk' },
 	{ value: 'bn', label: 'বাংলা' },
+	{ value: 'hu', label: 'Magyar' },
 ]
 
 export function LanguageSwitcher(props: ButtonProps) {
